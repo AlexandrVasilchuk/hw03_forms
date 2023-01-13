6f14ebbe2c6b6dcbe9683b616a80acf0a7cbf2c4
Àëêåêñàíäð Âàñèëü÷óê <alexandrvsko@yandex.ru>
2023-01-13 17:23:48 +0300

    reformate core/year annotation

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -3,7 +3,7 @@ import datetime
 from django.http import HttpRequest
 
 
-def year(request: HttpRequest) -> dict[str, datetime]:
+def year(request: HttpRequest) -> dict[str, int]:
     """Добавляет переменную с текущим годом.
     Args:
         request: Любой запрос на сайт,
