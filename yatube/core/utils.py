87406c2eecfe438d0c528ec5802bf7eb0816b40d
Àëêåêñàíäð Âàñèëü÷óê <alexandrvsko@yandex.ru>
2023-01-13 17:20:21 +0300

    reformate core/utils annotation

--- yatube/core/utils.py
+++ yatube/core/utils.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 from django.http import HttpRequest
 
 
-def paginate(request: HttpRequest, posts: Any) -> dict[str, Any]:
+def paginate(request: HttpRequest, posts: Any) -> Any:
     paginator = Paginator(posts, settings.LIMIT)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
